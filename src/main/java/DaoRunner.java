1092107d5f78ba00c61c14358147c6ea9c8aaf51
d-ops <annexm@ya.ru>
2022-10-18 11:43:55 +0300

    ticketDao (SELECT, UPDATE)

--- src/main/java/DaoRunner.java
+++ src/main/java/DaoRunner.java
@@ -2,16 +2,30 @@ import dao.TicketDao;
 import entity.Ticket;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 public class DaoRunner {
     public static void main(String[] args) {
+//        List<Ticket> tickets = TicketDao.getINSTANCE().findAll();
+//        System.out.println(tickets);
         TicketDao ticketDao = TicketDao.getINSTANCE();
-//        Ticket ticket = saveTicket(new Ticket());
-//        System.out.println(ticketDao.save(ticket));
-        System.out.println(ticketDao.delete(56L));
+        Optional<Ticket> maybeTicket = ticketDao.findById(3L);
+        System.out.println(maybeTicket);
+        maybeTicket.ifPresent(
+                ticket -> {
+                    ticket.setCost(BigDecimal.valueOf(188.88));
+                    ticketDao.update(ticket);
+                }
+        );
+        System.out.println(ticketDao.findById(3L));
+//        Ticket maybeTicket = saveTicket(new Ticket());
+//        System.out.println(ticketDao.save(maybeTicket));
+
+//        System.out.println(ticketDao.delete(56L));
     }
 
-    public static Ticket saveTicket(Ticket ticket){
+    public static Ticket saveTicket(Ticket ticket) {
         ticket.setPassengerNo("1234567");
         ticket.setPassengerName("Test");
         ticket.setFlightId(3L);
--- src/main/java/dao/TicketDao.java
+++ src/main/java/dao/TicketDao.java
@@ -5,6 +5,9 @@ import exception.DaoException;
 import util.ConnectionManager;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class TicketDao {
     private final String SQL_DELETE = """
@@ -14,6 +17,25 @@ public class TicketDao {
     private final String SQL_CREATE = """
             INSERT into ticket("passenger_no", passenger_name, flight_id, seat_no, cost) 
             values (?, ?, ?, ?, ?)
+            """;
+
+    private final String SQL_UPDATE = """
+            update ticket set
+            passenger_no = ?,
+            passenger_name = ?,
+            flight_id = ?,
+            seat_no = ?,
+            cost = ?
+            where id = ?
+            """;
+
+    private final String SQL_FIND_ALL = """
+            select id, passenger_no, passenger_name, flight_id, seat_no, cost
+            from ticket
+            """;
+
+    private final String SQL_FIND_BY_ID = SQL_FIND_ALL + """
+            where id = ?
             """;
 
 
@@ -27,6 +49,62 @@ public class TicketDao {
         return INSTANCE;
     }
 
+    public List<Ticket> findAll() {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_ALL)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Ticket> tickets = new ArrayList<>();
+            while (resultSet.next()) {
+                tickets.add(buildTicket(resultSet));
+            }
+            return tickets;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    public Optional<Ticket> findById(Long id) {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_BY_ID)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Ticket ticket = null;
+            if (resultSet.next()) {
+                ticket = buildTicket(resultSet);
+            }
+            return Optional.ofNullable(ticket);
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    private Ticket buildTicket(ResultSet resultSet) throws SQLException {
+        Ticket ticket = new Ticket(
+                resultSet.getLong("id"),
+                resultSet.getString("passenger_no"),
+                resultSet.getString("passenger_name"),
+                resultSet.getLong("flight_id"),
+                resultSet.getString("seat_no"),
+                resultSet.getBigDecimal("cost")
+        );
+        return ticket;
+    }
+
+    public void update(Ticket ticket) {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_UPDATE)) {
+            preparedStatement.setString(1, ticket.getPassengerNo());
+            preparedStatement.setString(2, ticket.getPassengerName());
+            preparedStatement.setLong(3, ticket.getFlightId());
+            preparedStatement.setString(4, ticket.getSeatNo());
+            preparedStatement.setBigDecimal(5, ticket.getCost());
+            preparedStatement.setLong(6, ticket.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
     public boolean delete(Long id) {
         try (Connection connection = ConnectionManager.get();
              PreparedStatement preparedStatement = connection.prepareStatement(SQL_DELETE);) {
@@ -37,10 +115,10 @@ public class TicketDao {
         }
     }
 
-    public Ticket save(Ticket ticket){
+    public Ticket save(Ticket ticket) {
         try (Connection connection = ConnectionManager.get();
-        PreparedStatement preparedStatement =
-                connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setString(1, ticket.getPassengerNo());
             preparedStatement.setString(2, ticket.getPassengerName());
             preparedStatement.setLong(3, ticket.getFlightId());
@@ -50,7 +128,7 @@ public class TicketDao {
             preparedStatement.executeUpdate();
 
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-            if (generatedKeys.next()){
+            if (generatedKeys.next()) {
                 ticket.setId(generatedKeys.getLong("id"));
             }
             return ticket;
